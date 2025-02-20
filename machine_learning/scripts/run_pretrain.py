c298e324c6a85c7d68c0ba7856e7827d217d5694
CashelF <cashel@utexas.edu>
2025-02-20 14:29:33 -0600

    Change pretrain example command file path

--- machine_learning/scripts/run_pretrain.py
+++ machine_learning/scripts/run_pretrain.py
@@ -24,4 +24,4 @@ if __name__ == "__main__":
         out_path=args.out_path
     )
     
-# python scripts/run_pretrain.py --root_dir data/mvlrs_v1 --epochs 5 --batch_size 64 --num_classes 28
+# python machine_learning/scripts/run_pretrain.py --root_dir machine_learning/data/mvlrs_v1/pretrain --epochs 5 --batch_size 64 --num_classes 28
